07c6c0dc7e70e359f6578ee7ea0414700a047c5e
Adriana Cabrera Escobar <160674673+AdriCabrera@users.noreply.github.com>
2024-04-05 22:12:39 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,37 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#My-excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hello Rigo from the console! ");
+};
+let generateExcuse = () => {
+  let who = ["Mi gato ", "Un niño ", "El vecino ", "Mi abuelita "];
+  let action = ["rompió ", "aplastó ", "pisó ", "botó "];
+  let what = ["mi portátil ", "mi tarea ", "mis documentos ", "mi auto "];
+  let when = [
+    "mientras dormía",
+    "en la mañana",
+    "mientras me ejercitaba",
+    "cuando salía de casa"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    "" +
+    action[actionIndex] +
+    "" +
+    what[whatIndex] +
+    "" +
+    when[whenIndex]
+  );
 };
